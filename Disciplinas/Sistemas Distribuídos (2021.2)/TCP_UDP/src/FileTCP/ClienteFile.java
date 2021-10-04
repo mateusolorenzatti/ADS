a846f2dad51dc75bb3a98af24677f6d2d3b9ff6b
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2021-10-03 21:44:48 -0300

    Exercicios 29/09

---
+++ Disciplinas/Sistemas Distribuídos (2021.2)/TCP_UDP/src/FileTCP/ClienteFile.java
@@ -0,0 +1,26 @@
+package FileTCP;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ClienteFile {
+    public static void main(String[] args) throws Exception {
+
+        Socket socket = new Socket("10.0.0.119", 5000);
+        byte[] contents = new byte[10000];
+
+        FileOutputStream fos = new FileOutputStream("e:\\Bookmarks1.html");
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        InputStream is = socket.getInputStream();
+
+        int bytesRead = 0;
+        while ((bytesRead = is.read(contents)) != -1)
+            bos.write(contents, 0, bytesRead);
+        bos.flush();
+        socket.close();
+        System.out.println("File saved successfully!");
+    }
+}
---
+++ Disciplinas/Sistemas Distribuídos (2021.2)/TCP_UDP/src/FileTCP/ServidorFile.java
@@ -0,0 +1,48 @@
+package FileTCP;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorFile {
+    public static void main(String[] args) throws Exception {
+        int PORT = 5000;
+
+        ServerSocket ssock = new ServerSocket(PORT);
+        System.out.println("\n\n*** Servidor aguardando na porta " + PORT);
+
+        Socket socket = ssock.accept();
+
+        File file = new File("e:\\Bookmarks.html");
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+
+        OutputStream os = socket.getOutputStream();
+
+        byte[] contents;
+        long fileLength = file.length();
+        long current = 0;
+
+        while (current != fileLength) {
+            int size = 10000;
+            if (fileLength - current >= size)
+                current += size;
+            else {
+                size = (int) (fileLength - current);
+                current = fileLength;
+            }
+            contents = new byte[size];
+            bis.read(contents, 0, size);
+            os.write(contents);
+            System.out.print("Sending file ... " + (current * 100) / fileLength + "% complete!");
+        }
+        os.flush();
+
+        socket.close();
+        ssock.close();
+        System.out.println("File sent succesfully!");
+    }
+}
