a7ec85a2387fb27b221810ec669bb9e7a97ab0e9
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2021-10-03 22:09:19 -0300

    Exercicios 03/10

--- Disciplinas/Sistemas Distribuídos (2021.2)/TCP_UDP/src/FileTCP/ClienteFile.java
+++ Disciplinas/Sistemas Distribuídos (2021.2)/TCP_UDP/src/FileTCP/ClienteFile.java
@@ -1,15 +1,21 @@
 package FileTCP;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class ClienteFile {
     public static void main(String[] args) throws Exception {
+        String fileName;
 
         Socket socket = new Socket("10.0.0.119", 5000);
+
+        BufferedReader inFromUSer = new BufferedReader(new InputStreamReader(System.in));
+
+        DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+        fileName = inFromUSer.readLine();
+        outToServer.writeBytes(fileName + "\n");
+
         byte[] contents = new byte[10000];
 
         FileOutputStream fos = new FileOutputStream("/home/mateusolorenzatti/desenv/IFRS/ADS/Disciplinas/Sistemas Distribuídos (2021.2)/TCP_UDP/clientFiles/teste.txt");
--- Disciplinas/Sistemas Distribuídos (2021.2)/TCP_UDP/src/FileTCP/ServidorFile.java
+++ Disciplinas/Sistemas Distribuídos (2021.2)/TCP_UDP/src/FileTCP/ServidorFile.java
@@ -1,45 +1,49 @@
 package FileTCP;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServidorFile {
     public static void main(String[] args) throws Exception {
         int PORT = 5000;
+        String requestedFile;
 
         ServerSocket ssock = new ServerSocket(PORT);
         System.out.println("\n\n*** Servidor aguardando na porta " + PORT);
 
         Socket socket = ssock.accept();
 
-        File file = new File("/home/mateusolorenzatti/desenv/IFRS/ADS/Disciplinas/Sistemas Distribuídos (2021.2)/TCP_UDP/serverFiles/teste.txt");
-        FileInputStream fis = new FileInputStream(file);
-        BufferedInputStream bis = new BufferedInputStream(fis);
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+        requestedFile = inFromClient.readLine();
 
-        OutputStream os = socket.getOutputStream();
+        System.out.printf("Arquivo solicitado: " + requestedFile);
 
-        byte[] contents;
-        long fileLength = file.length();
-        long current = 0;
-
-        while (current != fileLength) {
-            int size = 10000;
-            if (fileLength - current >= size)
-                current += size;
-            else {
-                size = (int) (fileLength - current);
-                current = fileLength;
-            }
-            contents = new byte[size];
-            bis.read(contents, 0, size);
-            os.write(contents);
-            System.out.print("Sending file ... " + (current * 100) / fileLength + "% complete!");
-        }
-        os.flush();
+//        File file = new File("/home/mateusolorenzatti/desenv/IFRS/ADS/Disciplinas/Sistemas Distribuídos (2021.2)/TCP_UDP/serverFiles/teste.txt");
+//        FileInputStream fis = new FileInputStream(file);
+//        BufferedInputStream bis = new BufferedInputStream(fis);
+//
+//        OutputStream os = socket.getOutputStream();
+//
+//        byte[] contents;
+//        long fileLength = file.length();
+//        long current = 0;
+//
+//        while (current != fileLength) {
+//            int size = 10000;
+//            if (fileLength - current >= size)
+//                current += size;
+//            else {
+//                size = (int) (fileLength - current);
+//                current = fileLength;
+//            }
+//            contents = new byte[size];
+//            bis.read(contents, 0, size);
+//            os.write(contents);
+//            System.out.print("Sending file ... " + (current * 100) / fileLength + "% complete!");
+//        }
+//        os.flush();
 
         socket.close();
         ssock.close();
